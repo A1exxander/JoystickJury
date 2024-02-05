350e4425e47fe86c958a4a1845c902528bae4365
A1exxander <88210134+A1exxander@users.noreply.github.com>
2024-02-04 19:43:51 -0500

    Delete src directory

--- src/components/FeaturedGames/FeaturedGamesCenter/IndexSelector/IndexSelectorListItem.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./IndexSelectorListItem.css";
-
-export default function IndexSelectorListItem({index, currentlySelectedIndex, setIndex}){
-    return (
-        (index === currentlySelectedIndex) ? <li className = "index-selector-list-item-active" onClick={() => setIndex(index)}></li> : <li className = "index-selector-list-item" onClick={() => setIndex(index)}></li>
-    );
-}
